ca8ed45897da0c791525f5cf904ace0fe9182b1b
Nithilan Rameshkumar <pigeongode@gmail.com>
2020-12-22 14:15:55 +0530

    Fixed bugs

--- pigeon.sh
+++ pigeon.sh
@@ -1,6 +1,6 @@
-pacman -S vim alsa-utils firefox xorg xorg-xinit xorg-server base-devel make git ranger xcompmgr xwallpaper pywal --noconfirm &&
+pacman -S vim alsa-utils firefox xorg xorg-xinit xorg-server base-devel make git ranger xcompmgr xwallpaper python3 python3-pip --noconfirm &&
+pip3 install pywal && 
  
-
 #compiles dwm and st(terminal)
 cd dwm-6.2/ &&
 make clean install &&
